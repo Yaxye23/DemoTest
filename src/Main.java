5ada0dbd045fa91b34d00305ad08f9162a8775f0
YaxyeMuxamed <102649411+YaxyeMuxamed@users.noreply.github.com>
2023-02-27 18:07:08 -0800

    john tricks

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Declaring ArrayList lol
+        // Declaring ArrayList.
         List<Book> listOfBooks = new ArrayList<>();
         Book javaInAction = new Book("Java in Action",
                 "Yaxye Muxamed", "Malable", new BigDecimal(600));
